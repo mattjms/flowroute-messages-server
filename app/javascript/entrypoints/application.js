1c84741369cb9a9d02e2f777bbfe2e52eb505dbf
Matt Smith <mattsmj@gmail.com>
2023-04-24 21:34:40 +0000

    add vue js to project with simple demo component

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,21 +1,10 @@
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
 console.log('Vite ⚡️ Rails')
-
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
-
 console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
 
+console.log("TWO")
 // Example: Load Rails libraries in Vite.
 //
-// import * as Turbo from '@hotwired/turbo'
+// import * as Turbo from '@hotwired/turbo-rails'
 // Turbo.start()
 //
 // import ActiveStorage from '@rails/activestorage'
@@ -26,3 +15,10 @@ console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify
 
 // Example: Import a stylesheet in app/frontend/index.css
 // import '~/index.css'
+
+
+import { createApp } from 'vue'
+import App from '../components/App.vue'
+
+const app = createApp(App)
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import FullReload from "vite-plugin-full-reload"
 import RubyPlugin from 'vite-plugin-ruby'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   clearScreen: false,
   plugins: [
     RubyPlugin(),
+    vue(),
     FullReload(["config/routes.rb", "app/views/**/*"], { delay: 300 }),
   ],
 })
