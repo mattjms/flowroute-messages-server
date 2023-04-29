34c0bfde410f2a91e6bd4a4def7bf37898680f44
Matt Smith <mattsmj@gmail.com>
2023-04-29 15:35:02 +0000

    include CSRF tokens on ajax requests

--- app/javascript/boot/axios.js
+++ app/javascript/boot/axios.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 
 const api = axios.create({ baseURL: '' })
 
+api.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').content
+
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios
   app.config.globalProperties.$api = api
